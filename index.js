3126ad3fcd7c6c1d8cfb7c3ab7956de6b638b8d4
isagi ego <isagiego299@gmail.com>
2025-01-16 00:48:00 +0600

    authentication and user collection completed without verify token

--- index.js
+++ index.js
@@ -13,6 +13,55 @@ app.get('/', (req, res) => {
     res.send('GigFlow is running');
 })
 
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v8zqf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // database Colllections 
+        const userCollection = client.db("GigFlow").collection("users");
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user)
+            res.send(result);
+        })
+        
+        app.get('/users', async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+
+        app.get('/users/:id', async (req, res) => { 
+            const id = req.params.id;
+            const user = await userCollection.findOne({ email: id });
+            res.send(user);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
