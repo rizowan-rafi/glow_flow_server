69132b11bd9ad710a6ca960ccdf1b794297b1101
isagi ego <isagiego299@gmail.com>
2025-01-26 08:12:39 +0600

    error status 401,400 added

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const verifyToken = (req, res, next) => {
     }
     const token = req.headers.authorization.split(' ')[1]
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-        if (err) return res.status(400).send({ message: 'invalid token' })
+        if (err) return res.status(400).send({ message: 'invalid token ' })
         req.decoded = decoded;
         next();
     })
